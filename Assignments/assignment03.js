4d32c33713be80e749f3ce4dddc968b7e3fe7b6c
cjjaquith <71270371+cjjaquith@users.noreply.github.com>
2020-10-13 13:49:00 -0400

    Update assignment03.js

--- Assignments/assignment03.js
+++ Assignments/assignment03.js
@@ -2,6 +2,40 @@ let elem = [];
 // assign the entire table row for hole 1 to a variable, elem
 elem[1]
   = document.getElementById("1");
+elem[2]
+  = document.getElementById("2");
+elem[3]
+  = document.getElementById("3");
+elem[4]
+  = document.getElementById("4");
+elem[5]
+  = document.getElementById("5");
+elem[6]
+  = document.getElementById("6");
+elem[7]
+  = document.getElementById("7");
+elem[8]
+  = document.getElementById("8");
+elem[9]
+  = document.getElementById("9");
+elem[10]
+  = document.getElementById("10");
+elem[11]
+  = document.getElementById("11");
+elem[12]
+  = document.getElementById("12");
+elem[13]
+  = document.getElementById("13");
+elem[14]
+  = document.getElementById("14");
+elem[15]
+  = document.getElementById("15");
+elem[16]
+  = document.getElementById("16");
+elem[17]
+  = document.getElementById("17");
+elem[18]
+  = document.getElementById("18");
 
 // display the number of children (all td elements)
 // console.log(elem.children.length);
@@ -11,6 +45,40 @@ elem[1]
 // assign a function to the + button
 elem[1].children[4].children[0].onclick 
   = function(){add1(elem[1]);};
+elem[2].children[4].children[0].onclick
+  = function() {add1(elem[2]);};
+elem[3].children[4].children[0].onclick
+  = function() {add1(elem[3]);};
+elem[4].children[4].children[0].onclick
+  = function() {add1(elem[4]);};
+elem[5].children[4].children[0].onclick
+  = function() {add1(elem[5]);};
+elem[6].children[4].children[0].onclick
+  = function() {add1(elem[6]);};
+elem[7].children[4].children[0].onclick
+  = function() {add1(elem[7]);};
+elem[8].children[4].children[0].onclick
+  = function() {add1(elem[8]);};
+elem[9].children[4].children[0].onclick
+  = function() {add1(elem[9]);};
+elem[10].children[4].children[0].onclick
+  = function() {add1(elem[10]);};
+elem[11].children[4].children[0].onclick
+  = function() {add1(elem[11]);};
+elem[12].children[4].children[0].onclick
+  = function() {add1(elem[12]);};
+elem[13].children[4].children[0].onclick
+  = function() {add1(elem[13]);};
+elem[14].children[4].children[0].onclick
+  = function() {add1(elem[14]);};
+elem[15].children[4].children[0].onclick
+  = function() {add1(elem[15]);};
+elem[16].children[4].children[0].onclick
+  = function() {add1(elem[16]);};
+elem[17].children[4].children[0].onclick
+  = function() {add1(elem[17]);};
+elem[18].children[4].children[0].onclick
+  = function() {add1(elem[18]);};
 
 // create an "add1" function
 function add1 (elem) {
